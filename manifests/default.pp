6ebc5817cdb696e9d7e04c3b539c6eb56ca573db
Paul Waring <paul.waring@manchester.ac.uk>
2014-01-15 10:40:23 +0000

    Working VirtualHost setup
    Ideally we would use the Puppet Apache module for this, as it is cleaner
    than copying configuration files across manually, but unfortunately it
    does not work in Gentoo.

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,4 +1,21 @@
-#portage::package { "net-dns/bind-tools":
-#  ensure => present,
-#}
+file { ['/home/vagrant/www', '/home/vagrant/www/moodle2']:
+  ensure => directory,
+  mode => 0644,
+}
 
+file { '/home/vagrant/www/moodle2/index.html':
+  ensure => present,
+  mode => 0644,
+  content => 'Moodle',
+}
+
+file { '/etc/apache2/vhosts.d/moodle2.conf':
+  ensure => present,
+  source => '/vagrant_vhosts/moodle2.conf',
+  notify => Service['apache2'],
+}
+
+service { 'apache2':
+  ensure => running,
+  enable => true,
+}
