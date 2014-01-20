89b2b3c9c18edbc29bb51910e5b01aae67fe5a3c
Paul Waring <paul.waring@manchester.ac.uk>
2014-01-20 16:18:31 +0000

    Execute upgrade script without interaction
    No need to run this manually, so can use 'vagrant up' and then come back
    when it is finished. Errors logged to shared folder for easier
    copy/paste.

--- scripts/moodle-import.sh
+++ scripts/moodle-import.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 /usr/bin/mysql -u moodle -D moodle < /home/vagrant/moodle.sql
+/usr/bin/php /home/vagrant/www/moodle2/htdocs/admin/cli/upgrade.php --non-interactive 2>&1 > /vagrant_log/upgrade.txt
